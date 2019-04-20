77e06dfa3c3097a2611f692c360cb23f8c0bf247
Zeynep <zynpogn81@gmail.com>
2019-04-20 16:17:12 +0300

    Django Girls uygulamam, ilk commit

---
+++ deneme.py
@@ -0,0 +1,67 @@
+print('merhaba,django girls')
+
+mylist=[24,2342,23423,23432,242]
+mylist.sort()
+print(mylist)
+print("Ters Sirali Hali:")
+mylist.reverse()
+print(mylist)
+
+
+
+if 2>3:
+	print("yanlis")
+else:
+	print("Digeri calismadi")
+
+name = 'zeynep'
+if name=='ayse':
+	print('Selam Ayse!')
+elif name=='zeynep':
+	print('selam zeynep!')
+else:
+	print('selam yabancı!')
+
+volume=57
+
+#dsjsdfhsjdkfhsdhfsjdkfhskdfhskdfhskjdfhkjhsdk
+
+
+def merhaba():
+	print("merhaba")
+
+
+
+
+merhaba()
+
+def toplama(x,y):
+
+	print(x+y)
+
+
+
+
+toplama(12,5)
+toplama(5,6)
+
+
+
+def isimBul(name):
+	if name=='Ayse':
+		print("merhaba Ayse")
+
+
+isimBul('Ayse')
+
+def hi(name):
+	print('merhaba'+name+ '!')
+girls=['sevda','kubra','neslihan']
+for name in girls:
+	hi (name)
+	print('siradaki kiz') #
+	
+	for i in range(1,6):
+		print(i)
+
+
